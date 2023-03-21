575f6e3bdc3ce16517875f94445220c092f4e12f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-21 00:11:27 +0000

    Added dark mode support for news widget

--- finWidget/src/RiskWidget.js
+++ finWidget/src/RiskWidget.js
@@ -1,50 +1,68 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useStockRisk } from './utils/Hooks';
-import {subscribe, unsubscribe} from "messagebusmono";
-import RiskWidgetPlaceholder from './components/RiskWidgetPlaceholder';
+import React, { useState, useEffect } from "react";
+import "./css/DynamicFinancialNewsWidget.css";
 
-function RiskWidget(props) {
-  const {uuid, symbol} = props;
-  const [currentSymbol, setCurrentSymbol] = useState("");
-  const { riskData, isLoading, error } = useStockRisk(currentSymbol);
-  const handleMessage = (event) => {
-    if(event.data.data.message && event.data.data.message.symbol && event.data.data.uuid === uuid)
-    setCurrentSymbol(event.data.data.message.symbol);
-  };
+const DynamicFinancialNewsWidget = (props) => {
+  const [news, setNews] = useState([]);
+  const { setWidgetStyle, widgetStyle, mode } = props;
 
   useEffect(() => {
-    subscribe(handleMessage);
-    return(() => {
-      unsubscribe(handleMessage);
-    })
-  },[]);
-
-  useEffect(() => {
-    if(!!symbol) {
-      setCurrentSymbol(symbol);
+    if (
+      !!window["widget-style"] &&
+      typeof setWidgetStyle === "function"
+    ) {
+      setWidgetStyle(window["widget-style"]);
     }
-  }, [symbol]);
+  }, []);
 
 
+  const newsList = [
+    {
+      id: 1,
+      title: "Stock Market Hits All-Time High",
+      description:
+        "The stock market reached a new record high today as investors cheered strong earnings reports and positive economic data.",
+      url: "/stock-market-update",
+    },
+    {
+      id: 2,
+      title: "Fed Keeps Interest Rates Steady",
+      description:
+        "The Federal Reserve announced that it will keep interest rates unchanged at its latest meeting, citing steady economic growth and low inflation.",
+      url: "/fed-interest-rates",
+    },
+    // more news items...
+  ];
 
-  if (isLoading) {
-    return <RiskWidgetPlaceholder />;
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  if (!riskData || !currentSymbol) {
-    return <div>Enter a symbol to see the risk data</div>;
-  }
+  useEffect(() => {
+    const fetchNews = () => {
+      const randomIndex = Math.floor(Math.random() * newsList.length);
+      const randomNews = newsList[randomIndex];
+      setNews([randomNews]);
+    };
 
+    const interval = setInterval(fetchNews, 5000);
+    fetchNews();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const modeClass = mode === "dark" ? "dark-mode" : "light-mode";
 
   return (
-    <div className="risk-container">
-      <div className="risk-symbol">{riskData.id}</div>
-      <div className="risk-risk">Risk: {riskData.risk}</div>
-      <div className="risk-description">{riskData.description}</div>
+    <div className={`dynamic-financial-news-widget ${modeClass}`}>
+      <div className={`news-item ${modeClass}`} style={{ width: "300px" }}>
+        {news.map((item) => (
+          <React.Fragment key={item.id}>
+            <h3 className={`news-item-title ${modeClass}`}>{item.title}</h3>
+            <p className={`news-item-description ${modeClass}`}>{item.description}</p>
+            <a className={`news-item-link`}>
+              Read More
+            </a>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default RiskWidget;
+export default DynamicFinancialNewsWidget;
