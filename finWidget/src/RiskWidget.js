0a8e9d7191ef933e0475bb2ea843f94c44868d22
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-06-05 00:36:15 +0100

    Risk widget messaging refactoring

--- finWidget/src/RiskWidget.js
+++ finWidget/src/RiskWidget.js
@@ -1,39 +1,34 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useStockRisk } from './utils/Hooks';
-import {subscribe, unsubscribe} from "messagebusmono";
-import RiskWidgetPlaceholder from './components/RiskWidgetPlaceholder';
+import React, { useCallback, useEffect, useState } from "react";
+import { useStockRisk } from "./utils/Hooks";
+import { subscribe, unsubscribe } from "messagebusmono";
+import RiskWidgetPlaceholder from "./components/RiskWidgetPlaceholder";
 
 function RiskWidget(props) {
-  const {uuid, symbol, mode} = props;
+  const { uuid, symbol, mode } = props;
   const [currentSymbol, setCurrentSymbol] = useState("");
   const { riskData, isLoading, error } = useStockRisk(currentSymbol);
+
   const handleMessage = (event) => {
-    if (
-      event?.data?.data?.message &&
-      event.data.data.message.symbol &&
-      event.data.data.uuid === uuid
-    ) {
-      setCurrentSymbol(event.data.data.message.symbol);
+    if (event.data.uuid === uuid) {
+      setCurrentSymbol(event.data.message.symbol);
     }
-  };  
+  };
 
   useEffect(() => {
     subscribe(handleMessage);
-    return(() => {
+    return () => {
       unsubscribe(handleMessage);
-    })
-  },[]);
+    };
+  }, []);
 
   useEffect(() => {
-    if(!!symbol) {
+    if (!!symbol) {
       setCurrentSymbol(symbol);
     }
   }, [symbol]);
 
-
-
   if (isLoading) {
-    return <RiskWidgetPlaceholder {...{mode}}/>;
+    return <RiskWidgetPlaceholder {...{ mode }} />;
   }
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -48,7 +43,9 @@ function RiskWidget(props) {
     <div className={`risk-container ${modeClass}`}>
       <div className={`risk-symbol  ${modeClass}`}>{riskData.id}</div>
       <div className={`risk-risk  ${modeClass}`}>Risk: {riskData.risk}</div>
-      <div className={`risk-description  ${modeClass}`}>{riskData.description}</div>
+      <div className={`risk-description  ${modeClass}`}>
+        {riskData.description}
+      </div>
     </div>
   );
 }
